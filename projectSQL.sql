8e829c5066a239fd6c0d0dc4570547eeac18a354
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-05-17 10:26:58 +0300

    File Syncing

---
+++ projectSQL.sql
@@ -0,0 +1,58 @@
+create table Student (
+    id int primary key,
+    username varchar(50) not null,
+    email varchar(50) not null,
+    password varchar(50) not null,
+    phone varchar(20),
+    address varchar(50),
+);
+
+create table Category (
+    categoryId int primary key,
+    categoryName varchar(20) not null
+);
+
+create table Admin(
+    id int primary key,
+    username varchar(50) not null,
+    email varchar(50) not null,
+    password varchar(50) not null,
+    phone varchar(20),
+    address varchar(50)
+);
+
+create table Book(
+    bookId int primary key,
+    ISBN varchar(50) unique,
+    borrowingFees int not null,
+    categoryId int,
+    title varchar(50) not null,
+    author varchar(50) not null
+    foreign key (categoryId) REFERENCES Category(categoryId)
+);
+
+create table Review(
+    reviewId int primary key,
+    studentId int,
+    comment text,
+    bookISBN varchar(50),
+    foreign key (bookISBN) references Book(ISBN),
+    foreign key (studentId) references Student(id)
+);
+
+create table Inventory(
+    numOfCopies int,
+    bookISBN varchar(50) unique,
+    adminId int
+    foreign key (adminId) REFERENCES Admin(id),
+    foreign key (bookISBN) references Book(ISBN),
+);
+
+create table Borrowed(
+    issueDate VARCHAR(10),
+    returnDate VARCHAR(10),
+    studentId int,
+    bookID int,
+    foreign key (studentId) references Student(id),
+    foreign key (bookId) references Book(bookId)
+);
